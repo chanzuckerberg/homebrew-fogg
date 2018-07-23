f4413304ecc29e6364401751df66a85b7708573a
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-23 14:01:04 -0400

    Brew formula update for fogg version v0.10.4

--- fogg.rb
+++ fogg.rb
@@ -3,7 +3,7 @@ class Fogg < Formula
   homepage "https://github.com/chanzuckerberg/fogg"
   url "https://github.com/chanzuckerberg/fogg/releases/download/v0.10.4/fogg_0.10.4_darwin_amd64.tar.gz"
   version "0.10.4"
-  sha256 "3d4878e6fecc6beb1c3937584834cf69c42112a9eba1ab07c770128e7558163a"
+  sha256 "ca6c200a7830c6557463d07266394e643fa2591862849fb36de83e66b64399ac"
 
   def install
     bin.install "fogg"
